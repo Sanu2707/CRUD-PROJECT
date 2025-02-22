24771720d27eec186d95ff713d174a443c9bc009
Sanu2707 <sanikagajarishi2004@gmail.com>
2025-02-22 14:14:29 +0530

    second commit

--- flipkart/clothing/models.py
+++ flipkart/clothing/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100, null=True)
-    description = models.TextField(null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     size = models.CharField(max_length=50, null=True)
     color = models.CharField(max_length=50, null=True)
--- flipkart/clothing/views.py
+++ flipkart/clothing/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from clothing.models import Product
+from clothing.forms import ProductForm
 
 # Create your views here.
 def clothing(request):
+    form = ProductForm()
     pro_img = Product.objects.all()
-    return render(request,'clothing/fashion.html',{'pro_img':pro_img})
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            
+    return render(request,'clothing/fashion.html',{'pro_img':pro_img, 'form':form})
     
